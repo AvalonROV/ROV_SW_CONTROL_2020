0a9b0dd74df0bb8209d71e88e5bef8f7e8584cc2
unknown <ben@familygriffiths.me.uk>
2020-02-09 17:36:27 +0000

    Added ESC thruster testing functionality including esc arming.

--- main/main.ino
+++ main/main.ino
@@ -1,53 +1,106 @@
 /**@main.ino */
 
+// IMPORTS
+#include <Servo.h>
+
 // PINOUT
+
+// ACTUATORS
 #define ACT_1 2
 #define ACT_2 3
 #define ACT_3 4
+#define ACT_4 5
+#define ACT_5 6
+#define ACT_6 7
+
+// THRUSTERS
+#define THRUST_1 10
+#define THRUST_2 9
+#define THRUST_3 9
+#define THRUST_4 9
+#define THRUST_5 9
+#define THRUST_6 9
+#define THRUST_7 9
+#define THRUST_8 9
 
 // PARAMETERS
-#define BUFFER_SIZE 10
+#define BUFFER_SIZE 40
 #define BAUD_RATE 115200
-#define ACTUATOR_QUANTITY 3
+#define ACTUATOR_QUANTITY 6
+#define THRUSTER_QUANTITY 8
+#define SENSOR_QUANTITY 3
 #define IDENTITY "AVALONROV"
 
-int actuatorList[ACTUATOR_QUANTITY] = {ACT_1, ACT_2, ACT_3};
+int actuatorList[ACTUATOR_QUANTITY] = {ACT_1, ACT_2, ACT_3, ACT_4, ACT_5, ACT_6};
+int thrusterList[THRUSTER_QUANTITY] = {THRUST_1, THRUST_2, THRUST_3, THRUST_4, THRUST_5, THRUST_6, THRUST_7, THRUST_8};
+Servo thrusters[THRUSTER_QUANTITY];
 
 void setup()
 {
   Serial.begin(BAUD_RATE);
   while (!Serial) {}
   Serial.flush();
-  
+
   // SETUP ACTUATORS PINS
   for (int i = 0; i < ACTUATOR_QUANTITY; i++) {
     pinMode(actuatorList[i], OUTPUT);
     digitalWrite(actuatorList[i], LOW);
   }
+
+  // SETUP THRUSTER PINS
+  for (int i = 0; i < THRUSTER_QUANTITY; i++) {
+    thrusters[i].attach(thrusterList[i]);
+    thrusters[i].writeMicroseconds(1000);
+  }
+
+  // INITIATE RANDOM NUMBER GENERATOR
+  randomSeed(1234);
 }
 
-/**
-  PURPOSE
-
-  Doxygen Test
-*/
-void setThrusters() {
-  Serial.println("@Setting Thrusters");
+int charArrayToInt(const char *data, size_t start, size_t finish) {
+  int val = 0;
+  while (start < finish) {
+    val = val * 10 + data[start++] - '0';
+  }
+  return val;
 }
 
-void setActuators(bool *actuatorStates) {
+void armThrusters() {
+  for (int i = 0; i < THRUSTER_QUANTITY; i++) {
+    thrusters[i].writeMicroseconds(1500);
+  }
+}
+
+void setThrusters(int *thrusterSpeeds) {
+  /*
+     PURPOSE
+     Sets the PWM for each thruster pin.
+
+     INPUTS
+     int *thrusterSpeeds = Name of the array that stores the processed thruster speeds (0-1023).
+
+     RETURNS
+     NONE
+  */
+  //Serial.println("@Setting Thrusters");
+  for (int i = 0; i < THRUSTER_QUANTITY; i++) {
+    thrusters[i].writeMicroseconds(map(thrusterSpeeds[i],1,999,1100,1900));
+  }
+}
+
+void setActuators(bool * actuatorStates) {
   /*
      PURPOSE
      Sets the actuator pins HIGH or LOW.
 
      INPUTS
-     bool *actuatorStates = Name of the array to stores the processed actuator states (true/false).
+     bool *actuatorStates = Name of the array that stores the processed actuator states (true/false).
 
      RETURNS
      NONE
   */
 
-  Serial.println("@Setting Actuators");
+  //Serial.println("@Setting Actuators");
 
   // LOOP THROUGH EACH ACTUATOR AND SET OUTPUT STATE
   for (int i = 0; i < ACTUATOR_QUANTITY; i++) {
@@ -60,7 +113,13 @@ void setCameras() {
 }
 
 void getSensors() {
-  //Serial.println("Getting Sensors");
+  for (int i = 0 ; i < SENSOR_QUANTITY ; i++) {
+    Serial.print(random(0, 100));
+    if (i != SENSOR_QUANTITY - 1)
+      Serial.print(",");
+  }
+  Serial.println();
+  //Serial.println("@Getting Sensors");
 }
 
 bool getSerialCommand(int bufferSize, char *receivedData) {
@@ -116,27 +175,28 @@ void processCommand(char *receivedData) {
   // CHECK IF THE COMMAND IS VALID
   if (receivedData[0] == '?') {
 
-    ///////////////////
-    /// ROV COMMAND ///
-    ///////////////////
+    // ROV COMMAND
     if (receivedData[1] == 'R') {
       //Serial.println("ROV COMMAND");
       switch (receivedData[2]) {
 
-        ///////////////////
-        // SET THRUSTERS //
-        ///////////////////
+        // SET THRUSTERS
         case 'T':
-          setThrusters();
+          // EXTRACT THRUSTER SPEEDS
+          int thrusterSpeeds[THRUSTER_QUANTITY];
+          for (int i = 0; i < THRUSTER_QUANTITY; i++) {
+            // CONVERT CHAR ARRAY TO INTEGERS
+            int thrustSpeed = charArrayToInt(receivedData, 3 + (3 * i), 6 + (3 * i));
+            thrusterSpeeds[i] = thrustSpeed;
+          }
+          setThrusters(thrusterSpeeds);
           break;
 
-        ///////////////////
-        // SET ACTUATORS //
-        ///////////////////
+        // SET ACTUATORS
         case 'A':
           // EXTRACT ACTUATOR STATES
           bool actuatorStates[ACTUATOR_QUANTITY];
-          for (int i = 0; i < sizeof(actuatorStates); i++) {
+          for (int i = 0; i < ACTUATOR_QUANTITY; i++) {
             if (receivedData[3 + i] == '1')
               actuatorStates[i] = true;
             else
@@ -145,18 +205,18 @@ void processCommand(char *receivedData) {
           setActuators(actuatorStates);
           break;
 
-        ///////////////////
-        /// SET CAMERAS ///
-        ///////////////////
+        // SET CAMERAS
         case 'C':
           setCameras();
           break;
 
-        ///////////////////
-        /// GET SENSORS ///
-        ///////////////////
-        case 'S' :
+        // GET SENSORS
+        case 'S':
           getSensors();
+          break;
+
+        case 'X':
+          armThrusters();
           break;
 
         // INVALID COMMAND
@@ -166,16 +226,12 @@ void processCommand(char *receivedData) {
       }
     }
 
-    //////////////////////
-    // MINI-ROV COMMAND //
-    //////////////////////
+    // MINI-ROV COMMAND
     else if (receivedData[1] == 'M') {
       //Serial.println("MINI ROV COMMAND");q
     }
 
-    //////////////////////
-    // IDENTITY REQUEST //
-    //////////////////////
+    // IDENTITY REQUEST
     else if (receivedData[1] == 'I') {
       Serial.println(IDENTITY);
     }
@@ -192,10 +248,10 @@ void processCommand(char *receivedData) {
 void loop() {
   // DEFINE ARRAY TO STORE RECEIVED DATA IN
   char receivedData[BUFFER_SIZE];
-  
+
   // UPDATE ARRAY WITH RECEIVED DATA
   bool readStatus = getSerialCommand(BUFFER_SIZE, receivedData);
-  
+
   // COMMAND SUCCESSFULLY RECEIVED
   if (readStatus)
   {
@@ -203,5 +259,4 @@ void loop() {
     //Serial.println(receivedData);
     processCommand(receivedData);
   }
-
 }
